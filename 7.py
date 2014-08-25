e4d2f933f28e8caa43535c9e3454a857a164a88d
darkdeymon <darkdeymon04@gmail.com>
2014-08-25 15:55:39 -0400

    Version 7

---
+++ 7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def cerrito(a):
+	for x in range(1,a+1):
+		imp=""
+		for i in range(1,x+1):
+			imp=imp+str(x)
+		print(imp)
+	for x in reversed(range(1,a)):
+		imp=""
+		for i in range(1,x+1):
+			imp=imp+str(x)
+		print(imp)
+cerrito(5)
