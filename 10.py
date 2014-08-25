7abe74de1ec951cdd0ecee576b8df1f816b522e7
darkdeymon <darkdeymon04@gmail.com>
2014-08-25 16:37:01 -0400

    Version 8

---
+++ 10.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+class circulo():
+	_ra=0
+	def __init__(self, r):
+		self._ra=r
+	def darArea(self):
+		return math.pi*self._ra
+c=circulo(5)
+print(c.darArea())
